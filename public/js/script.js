e90fa69340c71d28f9d9d0ce8f791918bc68497b
electricg <electric.g@gmail.com>
2014-05-14 01:09:25 +0100

    Header effect with SVG - first draft

--- public/js/script.js
+++ public/js/script.js
@@ -33,64 +33,27 @@
 })();
 
 
-// Show the black monitor full screen
+// Show the black monitor full screen - SVG
 (function () {
-  var $$ = document.querySelectorAll.bind(document);
+  if (supportsSVG()) {
+    var $s = document.getElementById('screen');
+    var $g = document.getElementById('giulia');
 
-  var $s = document.getElementById('screen');
-
-  var $logo = $$('.logo')[0];
-  var $logoImg = $$('.logo img')[0];
-  var $screenButton = $$('.screen-button')[0];
-  var $screenInt = $$('.screen')[0];
-  
-  window.mega = function() {
-    var diffHeight = 0;
-    var propHeight = 0;
-    var originalSize = {
-      w: $s.offsetWidth,
-      h: $s.offsetHeight
-    };
-    var winSize = {
-      w : window.innerWidth,
-      h: window.innerHeight
-    };
-    if (originalSize.h <= winSize.h) {
-      // do nothing, just resize the wrapper and vertical center the content
-      $s.style.width = winSize.w + 'px';
+    window.mega = function() {
+      var winSize = {
+        w : window.innerWidth,
+        h: window.innerHeight
+      };
       $s.style.height = winSize.h + 'px';
       $s.className += " " + 'js-mega';
-      $s.style.display = 'table-cell';
-      $s.style.verticalAlign = 'middle';
-    } else {
-      // $s.style.width = winSize.w + 'px';
-      // $s.style.height = winSize.h + 'px';
-      // $s.className += " " + 'js-mega';
+      $g.style.height = '100%';
 
-      // diffHeight = (originalSize.h - winSize.h) / 2;
-      // propHeight = winSize.h / originalSize.h;
-      // $screenInt.style.webkitTransform = 'scale(' + propHeight + ') translateY(-' + diffHeight + 'px)';
-
-
-
-      // $logo.style.marginTop = 0;
-      // $logo.style.marginBottom = 0;
-      // $screenButton.style.marginTop = 0;
-      // $screenButton.style.marginBottom = 0;
-    }
-    console.log(originalSize, winSize);
-  };
-  window.addEventListener('load', mega);
-
-  // https://github.com/typekit/webfontloader
-  WebFontConfig = {
-    custom: {
-      families: ['Proxima Nova']
-    },
-    active: function() {
-      // console.log('done');
-      // mega();
-    }
-  };
-
+      if (winSize.w < winSize.h) {
+        $g.style.width = '100%';
+      } else {
+        $g.style.width = 'auto';
+      }
+    };
+    mega();
+  }
 })();
