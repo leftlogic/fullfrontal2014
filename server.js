b2410e5145e46d696dbdb6ecae9b34433df0bb50
Remy Sharp <remy@remysharp.com>
2014-10-20 20:57:07 +0100

    Expose idify to compiled version

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var server = require('harp-static');
 var outputPath = __dirname + '/www';
 var port = process.env.PORT || 9000;
 
+global.idify = function (s) { return s.replace(/&.*?;/g, '').replace(/\s+/g, '-').replace(/[^\w\-]/g, '').toLowerCase(); };
+
 harp.compile(__dirname, outputPath, function(errors){
   if(errors) {
     console.log(JSON.stringify(errors, null, 2));
